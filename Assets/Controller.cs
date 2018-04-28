86c76c17e9b38dcff37abf6ede073ea4e1cee6b6
Vasanth Mohan <vmohan7@stanford.edu>
2018-04-27 20:02:46 -0700

    Inital Commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private Joycon j;
+    Moveable currMove = null;
+
+    void Start() {
+        j = JoyconManager.Instance.j[0];
+        if (j == null) {
+            Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate() {
+        gameObject.transform.rotation = Quaternion.Euler(90, 0, 0) * j.GetVector(); //switch coordinate systems
+
+        if (j.GetButtonDown(Joycon.Button.SHOULDER_1)) { //reset
+            // GetStick returns a 2-element vector with x/y joystick components
+            Debug.Log(string.Format("Stick x: {0:N} Stick y: {1:N}", j.GetStick()[0], j.GetStick()[1]));
+
+            // Joycon has no magnetometer, so it cannot accurately determine its yaw value. Joycon.Recenter allows the user to reset the yaw value.
+            j.Recenter();
+        }
+
+        if (j.GetButtonDown(Joycon.Button.SHOULDER_2)) { //capture
+            if(currMove != null) {
+                currMove.StopMove();
+                currMove = null;
+            } else {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+                    Moveable move = hit.transform.GetComponent<Moveable>();
+                    if (move != null) {
+                        move.StartMove(j);
+                        currMove = move;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Moveable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moveable : MonoBehaviour {
+    const float SPEED = 10f;
+    Joycon j = null;
+
+    public void StartMove(Joycon controller) {
+        j = controller;
+    }
+
+    public void StopMove() {
+        j = null;
+    }
+
+    void Update() {
+        if (j != null) {
+            j.SetRumble(160, 320, 0.3f, 200);
+
+            if (j.GetButton(Joycon.Button.DPAD_UP)) {
+                transform.position += (Vector3.forward * SPEED * Time.deltaTime);
+            }
+
+            if (j.GetButton(Joycon.Button.DPAD_DOWN)) {
+                transform.position += (Vector3.back * SPEED * Time.deltaTime);
+            }
+
+            if (j.GetButton(Joycon.Button.DPAD_LEFT)) {
+                transform.position += (Vector3.left * SPEED * Time.deltaTime);
+            }
+
+            if (j.GetButton(Joycon.Button.DPAD_RIGHT)) {
+                transform.position += (Vector3.right * SPEED * Time.deltaTime);
+            }
+        }
+    }
+}
